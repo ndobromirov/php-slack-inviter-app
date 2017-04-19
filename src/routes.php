055e31cc3c964555b9ba663d541f3a15d54a49f4
Nikolay Dobromirov <nikolay.dobromirov@ffwagency.com>
2017-04-20 00:09:03 +0300

    Added slack-inviter library. Implemented the positive workflow.

--- src/routes.php
+++ src/routes.php
@@ -2,6 +2,11 @@
 
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
+use PhpSlackInviter\InviteHandler;
+
+// TODO: Add CSRF.
+// TODO: Add validations.
+// TODO: Update tests.
 
 $app->get('/', function (RequestInterface $request, ResponseInterface $response) {
     $settings = $this->get('settings');
@@ -16,9 +21,25 @@ $app->get('/', function (RequestInterface $request, ResponseInterface $response)
 });
 
 $app->post('/', function (RequestInterface $request, ResponseInterface $response) {
-    $post = $request->getParsedBody();
 
-    return $response
-        ->withStatus(302)
-        ->withHeader('Location', '/');
+    $email = $request->getParsedBody()['email'];
+
+    //
+    $settings = $this->get('settings');
+    $team = $settings['slack_team_name'];
+    $token = $settings['slack_access_token'];
+
+    try {
+        $handler = new InviteHandler($token, $team);
+        $handler->requestNewInvite($email);
+        return $response->withStatus(302)->withHeader('Location', '/success');
+    }
+    catch (\PhpSlackInviter\Exception $e) {
+        // TODO: Add flash message for the error page.
+        echo $e->getMessage();
+    }
+});
+
+$app->get('/success', function($request, $response) {
+    return $this->view->render($response, 'success');
 });
